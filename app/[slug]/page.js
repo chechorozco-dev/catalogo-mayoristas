353983d4e881f5627f3e76fb3b25fb074fb627bc
chechorozco-dev <chechorozco@gmail.com>
2026-09-08 14:24:27 -0500

    Update page.js

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -21,16 +21,16 @@ export default async function TiendaPage({ params }) {
     .select(`
       precio_personalizado,
       visible,
-     productos (
-  id,
-  referencia,
-  nombre,
-  categoria,
-  descripcion,
-  foto_url,
-  foto_url_2,
-  precio_detal
-)
+      productos (
+        id,
+        referencia,
+        nombre,
+        categoria,
+        descripcion,
+        foto_url,
+        foto_url_2,
+        precio_detal
+      )
     `)
     .eq("tienda_id", tienda.id)
     .eq("visible", true);
@@ -46,6 +46,7 @@ export default async function TiendaPage({ params }) {
     categoria: item.productos.categoria,
     descripcion: item.productos.descripcion,
     foto_url: item.productos.foto_url,
+    foto_url_2: item.productos.foto_url_2,
     precio:
       item.precio_personalizado ??
       item.productos.precio_detal,
