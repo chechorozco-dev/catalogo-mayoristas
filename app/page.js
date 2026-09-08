939229863bb489140374bbd979038c3ba1c3e6e0
chechorozco-dev <chechorozco@gmail.com>
2026-09-08 13:08:52 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main>
+      <section className="hero">
+        <p className="eyebrow">Catálogo digital</p>
+
+        <h1>Detalles que te hacen brillar</h1>
+
+        <p className="subtitle">
+          Descubre nuestros productos y arma tu pedido fácilmente.
+        </p>
+
+        <a className="button" href="/maria-accesorios">
+          Ver catálogo
+        </a>
+      </section>
+    </main>
+  );
+}
